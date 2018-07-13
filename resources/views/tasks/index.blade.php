27a0c69e9528fc6f961d6937b2b7f0e2886ed528
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2018-07-13 04:25:42 +0000

    Procfile

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,16 +3,37 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
+  <div class="row">
+   <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+       
     <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }} - {{ $task->status }}</li>
-            @endforeach
-        </ul>
+    
+        <table class="table table-striped">
+          <thead>
+             <tr>
+                <th>id</th>
+                <th>コンテンツ</th>
+                <th>ステータス</th>
+             </tr>
+          </thead>
+        　<tbody>
+            　@foreach ($tasks as $task)
+            　  <tr>
+    　　            <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+        　　        <td>{{ $task->content }}</td>
+            　　    <td>{{ $task->status }}</td>
+                </tr>
+              @endforeach
+        　</tbody>
+        </table>
+        
     @endif
     
-    {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-info']) !!}
+   
+   </div>
+  </div> 
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,14 +3,32 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
+  <div class="row">
+   <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+    
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>タスク: {{ $task->content }}</p>
-    <p>ステータス: {{ $task->status }}</p>
+    <table class="table table-hover">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>コンテンツ</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+    </table>
     
-    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-info']) !!}
         
+   </div>
+  </div> 
+  
 @endsection
